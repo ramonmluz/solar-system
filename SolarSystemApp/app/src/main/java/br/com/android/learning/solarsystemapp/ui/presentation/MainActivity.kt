e9a0ceb19be3a0dc4b3e27ee8348f4e1fdbef150
ramonhome <rmluzz@gmail.com>
2023-03-08 00:06:12 -0300

    [@ramonmluz] - Create image background to home solar system

--- SolarSystemApp/app/src/main/java/br/com/android/learning/solarsystemapp/ui/presentation/MainActivity.kt
+++ SolarSystemApp/app/src/main/java/br/com/android/learning/solarsystemapp/ui/presentation/MainActivity.kt
@@ -3,13 +3,17 @@ package br.com.android.learning.solarsystemapp.ui.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import br.com.android.learning.solarsystemapp.R
 import br.com.android.learning.solarsystemapp.ui.theme.SolarSystemAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +21,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             SolarSystemAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                HomeBackground()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HomeBackground() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Black)) {
+        Image(
+            painter = painterResource(id = R.drawable.background_solar_system),
+            contentDescription = stringResource(id = R.string.background_solar_system_description),
+            contentScale = ContentScale.Crop,
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true
+)
 @Composable
 fun DefaultPreview() {
     SolarSystemAppTheme {
-        Greeting("Android")
+        HomeBackground()
     }
 }
